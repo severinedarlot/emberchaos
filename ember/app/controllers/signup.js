83d35b8c9470d614f05b36c59febe78ceb9c8b12
Séverine Darlot <severine.darlot@gmail.com>
2014-10-19 23:59:13 +0200

    Add session authentification when sign up succeed

--- ember/app/controllers/signup.js
+++ ember/app/controllers/signup.js
@@ -1,18 +1,25 @@
 import Ember from 'ember';
 
 export default Ember.ObjectController.extend({
+
   actions: {
     save: function() {
-    	var self = this;
+    	var self = this, localPassword = this.get('password');
 
-			function transitionToPost() {
-				self.transitionToRoute('account');
+			function login(user) {
+				self.get('session').authenticate('simple-auth-authenticator:devise', {
+          identification: user.get('email'),
+          password: localPassword
+        }).then(function () {
+				  self.transitionToRoute('account');
+				});
+				localPassword = '';
 			}
 
 			function failure() {
 			}
 
-      this.get('model').save().then(transitionToPost).catch(failure);
+      this.get('model').save().then(login).catch(failure);
     }
   }
 });
