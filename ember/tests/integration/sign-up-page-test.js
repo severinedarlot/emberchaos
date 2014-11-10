960302b066a21c7aecbf6bdb3c21b863518e04f6
Séverine Darlot <severine.darlot@gmail.com>
2014-11-10 22:26:05 +0100

    Test: access to Sign up

--- ember/tests/integration/sign-up-page-test.js
+++ ember/tests/integration/sign-up-page-test.js
@@ -12,8 +12,17 @@ module('Integration - Sign up', {
 });
 
 
-test('Landing page has link to sign up', function() {
+test('Landing page allows to access to sign up', function() {
   visit('/').then(function() {
     equal(find('a:contains("Sign up")').length, 1);
+    click('a:contains("Sign up")').then(function () {
+      equal(find('h3:contains("Join EmberChaos")').length, 1);
+    });
+  });
+});
+
+test('Should be access directly', function() {
+  visit('/signup').then(function() {
+    equal(find('h3:contains("Join EmberChaos")').length, 1);
   });
 });
