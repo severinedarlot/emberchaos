4950385b27e53f454cb0552af34b1a872cf11e95
thsdrouot <ths.drouot@gmail.com>
2014-11-14 18:50:42 +0100

    Formulaire d'enregistrement de son véhicule, résolution des problèmes de conflit entre model (ember) et model (du véhicule)

--- ember/app/controllers/vehicles.js
+++ ember/app/controllers/vehicles.js
@@ -1,9 +1,13 @@
 import Ember from 'ember';
 
-export default Ember.ObjectController.extend({
+export default Ember.ArrayController.extend({
 	actions:{
 		createNewVehicle: function(){
-			this.get('store').createRecord('vehicle', {name:'Titine', type:'Voiture',brand:'Renault',model:'508'});
+			this.get('store').createRecord('vehicle', {name:this.get('name'), type:this.get('type'), brand:this.get('brand'), model:this.get('car_model')});
 		} 
+
+		// deleteVehicle:function(){
+		// 	this.get('store').deleteRecord('vehicle');
+		// }
 	}
 });
--- ember/app/routes/vehicles.js
+++ ember/app/routes/vehicles.js
@@ -4,10 +4,6 @@ export default Ember.Route.extend({
 	model: function(){
 		return this.store.find('vehicle');
 	}
-
-	// setupController: function(controller, model) {
-	//   controller.set('model', model);
-	// }
 });
 
 
