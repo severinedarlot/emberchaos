8ab7017210bf796c66fbe89c5326aec1f3d29ca3
Séverine Darlot <severine.darlot@gmail.com>
2014-12-01 10:00:54 +0100

    Oups, forgot to delete some files + update experts

--- ember/app/routes/vehicles/edit.js
+++
@@ -1,9 +0,0 @@
-import Ember from 'ember';
-// import AuthenticatedRouteMixin from 'simple-auth/mixins/authenticated-route-mixin';
-
-export default Ember.Route.Extend({
-  model: function (params) {
-    return this.store.find('vehicle', params.vehicle_id);
-  }
-});
-
