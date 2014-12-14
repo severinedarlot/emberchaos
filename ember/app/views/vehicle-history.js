1b9cbefb6aada8026f49b9852487abef01ba3c2f
Thomas <ths.drouot@gmail.com>
2014-12-14 11:24:24 +0100

    Mise en place de l'historique des réparations

--- ember/app/views/vehicle-history.js
+++ ember/app/views/vehicle-history.js
@@ -3,8 +3,11 @@ import Ember from "ember";
 export default Ember.View.extend({
   templateName: 'vehicle-history',
   service: "Vidange",
-  provider: "Thomas D.",
-  advise: "Prenez un criquet et placer le sous le capot",
-  offer: "Pneu chez Oscaro, Pneu chez Allopneu"
+  dateOfExecution : "27/11/2015",
+  expectedDateOfExecution : "25/12/2016",
+  serviceProvider: "Thomas D.",
+  advise: "Faire attention à la rouille sous le capot",
+  offer: "Pneu chez Oscaro -50%, Pneu chez Allopneu -30%",
+  invoice: "Facture du ",
 });
  
